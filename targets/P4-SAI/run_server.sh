90cca54f7654ac8733ce3af7041705ba537e7784
Yonatan Piasetzky <yonatanp@mellanox.com>
2017-01-11 21:08:21 +0200

    fixed run_server sudo issue

--- targets/P4-SAI/run_server.sh
+++ targets/P4-SAI/run_server.sh
@@ -1,5 +1,6 @@
 set -m
 rm -rf log.txt
+sudo -v
 sudo ./simple_switch -i 0@veth0 -i 1@veth2 -i 2@veth4 -i 3@veth6 -i 4@veth8 -i 5@veth10 -i 6@veth12 -i 7@veth14 --log-file log --log-flush sai.json &
 sleep 1
 ./runtime_CLI < p4src/DefaultConfig.txt
