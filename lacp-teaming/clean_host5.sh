82b73f4a6e62eedb3c29e1ab1cc3f627ebf1fd4a
yonatanp <yonatanp@mellanox.com>
2017-04-22 11:12:00 +0300

    fixed clean host5 script with new port

--- lacp-teaming/clean_host5.sh
+++ lacp-teaming/clean_host5.sh
@@ -1,3 +1,4 @@
  sudo ip netns exec host5 teamd -k -t team2
  sudo ip netns exec host5 ip link set host_port0 up
  sudo ip netns exec host5 ip link set host_port1 up
+ sudo ip netns exec host5 ip link set host_port2 up
