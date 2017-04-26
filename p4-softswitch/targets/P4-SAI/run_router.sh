c8ecafbd9e3743055716af0a07a413e62d146a49
yonatanp <yonatanp@mellanox.com>
2017-04-26 19:05:16 +0300

    run_router gets a different port for thrift

--- p4-softswitch/targets/P4-SAI/run_router.sh
+++ p4-softswitch/targets/P4-SAI/run_router.sh
@@ -1,7 +1,7 @@
 set -m
 rm -rf logs/router_log.txt
 sudo -v
-sudo ip netns exec sw_net ./simple_switch -i 0@router_port1 --log-file logs/router_log --log-flush --pcap sai_router.json &
+sudo ip netns exec sw_net ./simple_switch -i 0@router_port1 --log-file logs/router_log --log-flush --pcap --thrift-port 9091 sai_router.json &
 sleep 2
 ./runtime_CLI --pre SimplePreLAG < p4src/router/DefaultConfig.txt
 # ./sswitch_CLI < p4src/router/DefaultConfigMirror.txt
