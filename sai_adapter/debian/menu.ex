99cab7909276994e69c99d274932314f495edf59
yonatanp <yonatanp@mellanox.com>
2017-08-21 20:36:19 +0300

    added debian dir. also changed thrift_src to noinst_library

---
+++ sai_adapter/debian/menu.ex
@@ -0,0 +1,2 @@
+?package(p4-sai-adapter):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="p4-sai-adapter" command="/usr/bin/p4-sai-adapter"
---
+++ sai_adapter/debian/p4-sai-adapter.default.ex
@@ -0,0 +1,10 @@
+# Defaults for p4-sai-adapter initscript
+# sourced by /etc/init.d/p4-sai-adapter
+# installed at /etc/default/p4-sai-adapter by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
