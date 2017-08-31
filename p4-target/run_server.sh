8705fde73c770a148171c6ed0de9f9f3ebf36a98
yonatanp <yonatanp@mellanox.com>
2017-08-31 19:56:04 +0300

    sai_adapter changed to dynamic library

--- p4-target/run_server.sh
+++ p4-target/run_server.sh
@@ -12,7 +12,7 @@ sudo ip netns exec sw_net ./simple_switch -i 0@router_port1 -i 8@router_cpu_port
 export ROUTER_PID=$!
 sudo ip netns exec sw_net ./simple_switch -i 0@sw_port0 -i 1@sw_port1 -i 2@sw_port2 -i 3@sw_port3 -i 4@sw_port4 -i 5@sw_port5 -i 6@sw_port6 -i 7@sw_port7 -i 8@cpu_port -i 9@router_port0 --log-file logs/bridge_log --log-flush --pcap sai_bridge.json &
 export BRIDGE_PID=$!
-sleep 2
+sleep 10
 ./runtime_CLI --pre SimplePreLAG < p4src/bridge/DefaultConfig.txt
 ./sswitch_CLI < p4src/bridge/DefaultConfigMirror.txt
 ./runtime_CLI --pre SimplePreLAG --thrift-port 9091 < p4src/router/DefaultConfig.txt
