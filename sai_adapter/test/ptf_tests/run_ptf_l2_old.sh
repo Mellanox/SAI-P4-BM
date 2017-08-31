d12584f35c8cf2cd75d029663a07771f2a835bdb
yonatanp <yonatanp@mellanox.com>
2017-08-31 20:04:49 +0300

    changed libp4sai name to libsai

--- sai_adapter/test/ptf_tests/run_ptf_l2_old.sh
+++
@@ -1,3 +0,0 @@
-sudo ./ptf/ptf --test-dir tests_old/ --pypath sai_thrift_src/gen-py --test-params port_map_file=\"port_map.ini\" --interface 0@host_port0 --interface 1@host_port1 --interface 2@host_port2 \
-    	 --interface 3@host_port3 --interface 4@host_port4 --interface 5@host_port5 --interface 6@host_port6 --interface 7@host_port7  \
-    	 sail2_new.L2AcceptedFrameType sail2_new.L21DBridgeBasicTest sail2_new.L21QBridgeAccess2AccessTest sail2_new.L21QBridgeAccess2TrunkTest sail2_new.L21QBridgeTrunk2AccessTest sail2_new.L21QBridgeTrunk2TrunkTest sail2_new.L21DLagTest sail2_new.L21QLagTest
