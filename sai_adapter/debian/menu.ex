b60ccc63bfb8129c575a35182244249311be9603
yonatanp <yonatanp@mellanox.com>
2017-09-01 22:56:39 +0300

    deleted old dpkg output files from sai_adapter

--- sai_adapter/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(p4-sai-adapter):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="p4-sai-adapter" command="/usr/bin/p4-sai-adapter"
--- sai_adapter/debian/p4-sai-adapter.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the p4-sai-adapter package
-#
-0 4	* * *	root	[ -x /usr/bin/p4-sai-adapter_maintenance ] && /usr/bin/p4-sai-adapter_maintenance
